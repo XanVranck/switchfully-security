84a29246cf984a09a3c31658fb49be5a1983f658
Xan Vranckaert <xan.vranckaert@cegeka.com>
2018-09-07 13:00:59 +0200

    authorisatie - rollen enzo

--- src/main/java/com/cegeka/switchfully/security/ArmyResource.java
+++ src/main/java/com/cegeka/switchfully/security/ArmyResource.java
@@ -1,5 +1,6 @@
 package com.cegeka.switchfully.security;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ public class ArmyResource {
     public static final String ARMY_RESOURCE_PATH = "/armies";
 
     @RequestMapping(method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE ,path = "/{country}")
+    @PreAuthorize("hasAnyRole('PRIVATE', 'GENERAL')")
     public ArmyInfoDto getDeployedArmyInfo(@PathVariable(value = "country") String country){
         return ArmyInfoDto.armyInfoDto()
                 .withCountry(country)
@@ -23,22 +25,47 @@ public class ArmyResource {
     }
 
     @RequestMapping(method = RequestMethod.POST)
+    @PreAuthorize("hasRole('CIVILIAN')")
     public void joinArmy(){
         //TODO
     }
 
     @RequestMapping(method = RequestMethod.POST, path = "/promote/{name}")
+    @PreAuthorize("hasRole('HUMAN_RELATIONSHIPS')")
     public void promotePrivate(@PathVariable(value = "name") String name){
         //TODO
     }
 
     @RequestMapping(method = RequestMethod.POST, path = "/discharge/{name}")
+    @PreAuthorize("hasRole('HUMAN_RELATIONSHIPS')")
     public void dischargeSoldier(@PathVariable(value = "name") String name){
         //TODO
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/nuke")
+    @PreAuthorize("hasRole('GENERAL')")
     public String launchNukes(){
         return "The world ends. Not with a bang but a whimper";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/tanks")
+    @PreAuthorize("hasAnyRole('GENERAL', 'PRIVATE', 'CIVILIAN')")
+    public String getTanksInfo(){
+        return "all tanks";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/tanks")
+    @PreAuthorize("hasAnyRole('PRIVATE')")
+    public void addTanks(){
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "/tanks")
+    @PreAuthorize("hasAnyRole('GENERAL')")
+    public void blowUpTanksAndEnjoyTheFireworks(){
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, path = "/tanks")
+    @PreAuthorize("hasAnyRole('GENERAL', 'PRIVATE')")
+    public void addNewTanks(){
     }
 }
--- src/main/java/com/cegeka/switchfully/security/SecurityConfig.java
+++ src/main/java/com/cegeka/switchfully/security/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.cegeka.switchfully.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +15,7 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -21,6 +24,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/armies/**").hasRole("PRIVATE")
+//                .antMatchers("/nuke").hasRole("GENERAL")
+//                .antMatchers("/join").hasRole("CIVILIAN")
+//                .antMatchers("/promote/**").hasRole("HUMAN_RELATIONSHIPS")
+//                .antMatchers("/discharge/**").hasRole("HUMAN_RELATIONSHIPS")
+//                .antMatchers(HttpMethod.POST, "/tanks").hasRole("PRIVATE")
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().httpBasic()
